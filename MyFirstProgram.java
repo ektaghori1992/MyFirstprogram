2576a4fa2c33ec746ed900a8970c2b03362872b9
ekta <aektapatel0102@gmail.com>
2022-07-19 20:55:51 +0100

    my first java prg

---
+++ MyFirstProgram.java
@@ -0,0 +1,8 @@
+package JavaConcepts;
+
+public class MyFirstProgram {
+    public static void main(String[] args){
+        System.out.println("My First Aplication1");
+        System.out.println("Tester Test");
+    }
+}
