096348a7f2c6908f177e937e65d6faba954623e1
ekta <aektapatel0102@gmail.com>
2022-07-19 21:08:20 +0100

    its updated

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -3,6 +3,6 @@ package JavaConcepts;
 public class MyFirstProgram {
     public static void main(String[] args){
         System.out.println("My First Aplication1");
-        System.out.println("Tester Test");
+        System.out.println("Tester Test ekta tester testing github");
     }
 }
